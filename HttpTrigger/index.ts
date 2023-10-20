4ac7dd9c8d206b8c40b833ac9fe0ab4442ba6480
pronk02 <jmcdonald@hmsmarketingservices.com>
2023-10-20 10:30:21 -0400

    Refactored getSecrets function
    Refactored the `getSecrets` function in the `HttpTrigger/index.ts` file to accept a `nsEnvironment` parameter and added validation for the parameter. Also updated the return type of the function to include a string in case of invalid environment parameter.

--- HttpTrigger/index.ts
+++ HttpTrigger/index.ts
@@ -1,45 +1,43 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import { DefaultAzureCredential } from "@azure/identity";
 import { SecretClient } from "@azure/keyvault-secrets";
+import { ISecrets } from "./types";
 import dotenv from "dotenv";
 dotenv.config();
 
 const credential = new DefaultAzureCredential();
-const vaultName = "netsuite-oauth1-kv";
-const url = `https://${vaultName}.vault.azure.net`;
-const kvClient = new SecretClient(url, credential);
-const apiAccessToken = "apiAccessToken";
-
-interface ISecrets {
-    apiAccessToken: string;
-}
-
-async function getSecrets(): Promise<ISecrets> {
-    const token = (await kvClient.getSecret(apiAccessToken)).value;
-    console.log(`Latest version of the secret ${apiAccessToken}: `, token);
-
-    return {
-        apiAccessToken: token,
-    };
-    //const specificSecret = await client.getSecret(apiAccessToken, { version: latestSecret.properties.version! });
-    //console.log(`The secret ${apiAccessToken} at the version ${latestSecret.properties.version.value!}: `, specificSecret);
-}
 
 const httpTrigger: AzureFunction = async function (
     context: Context,
     req: HttpRequest
 ): Promise<void> {
     context.log("HTTP trigger function processed a request.");
-    const secrets = await getSecrets() //req.query.name || (req.body && req.body.name);
-    const responseMessage = secrets
-        ? `The secrets are: ${JSON.stringify(secrets)}.
-        This HTTP triggered function executed successfully.`
-        : `This HTTP triggered function executed successfully.`;
-
+    context.log("req.query: ", req.query);
+    const nsEnvironment = req.query.environment;
+    const secrets = await getSecrets(nsEnvironment);
+    const response = secrets ? JSON.stringify(secrets) : `No secrets were returned.`;
+    context.log("response: ", response);
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage,
+        body: response
     };
+
+    async function getSecrets(nsEnvironment = "sandbox"): Promise<ISecrets | string> {
+        if (nsEnvironment != "sandbox" && nsEnvironment != "production" && nsEnvironment != "") {
+            return "The 'environment' paramater must be either 'sandbox' or 'production'."
+        }
+        const url = `https://${nsEnvironment == "production" ? "hms-netsuite-prod-kv" : "hms-netsuite-sandbox-kv"}.vault.azure.net`;
+        const kvClient = new SecretClient(url, credential);
+        const realm = (await kvClient.getSecret("realm")).value;
+        return {
+            tokenId: (await kvClient.getSecret("token-id")).value,
+            tokenSecret: (await kvClient.getSecret("token-secret")).value,
+            consumerKey: (await kvClient.getSecret("consumer-key")).value,
+            consumerSecret: (await kvClient.getSecret("consumer-secret")).value,
+            realm: realm,
+            restServicesUrl: `https://${realm.replace("_", "-")}.suitetalk.api.netsuite.com`,
+            resletUrl: `https://${realm.replace("_", "-")}.restlets.api.netsuite.com`
+        };
+    }
 };
 
 export default httpTrigger;
---
+++ HttpTrigger/types.ts
@@ -0,0 +1,9 @@
+export interface ISecrets {
+    tokenId: string;
+    tokenSecret: string;
+    consumerKey: string;
+    consumerSecret: string;
+    realm: string,
+    restServicesUrl: string,
+    resletUrl: string
+}
